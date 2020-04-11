017114be6efc9626f19b8c55cdc8c2261afbcddb
gunjan <gunjansokal07@gmail.com>
2020-04-11 16:41:30 +0530

    middleware & logger

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const redux = require('redux');
+const reduxLogger = require('redux-logger');
+
 const createStore = redux.createStore;
 const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+// const applyMiddleware =redux.applyMiddleware();
+// when you describe the const u dont use () only redux.applyMiddleWare
+
+const logger = reduxLogger.createLogger();
 
 const BUY_CAKE = 'BUY_CAKE';
 const BUY_ICECREAME = 'BUY_ICECREAME';
@@ -52,13 +59,13 @@ const rootReducer = combineReducers({
     iceCreame: iceCreameReducer
 })
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
  
 // console.log(`Initial State: ${store.getState()}`);
 console.log('Initial State:', store.getState());
 
 const unsubscribe = store.subscribe(() => {
-    console.log('Updated State: ', store.getState());
+    // console.log('Updated State: ', store.getState());
     // console.log(`Updated State: ${store.getState()}`);
 });
 
